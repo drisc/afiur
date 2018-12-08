f4aef829cdec89f322bf22dd2ce09fa374970d8b
Cory Driscoll <cory@drisc.io>
2018-12-08 13:20:21 +0000

    [afiur] Fixed issue with date changing at 12pm

--- afiur.js
+++ afiur.js
@@ -1,10 +1,10 @@
 'use strict'
 
 function afiur () {
-  const origin = new Date('1994-11-03')
-  const current = new Date
-  const trueDays = Math.round(Math.abs(origin - current) / 8.64e7);
-  
+  //const origin = Date.UTC(1994,10,3)
+  const current = new Date().getTime()
+  const trueDays = ((current - origin) / 8.64e7).toFixed(0)
+
   const decimalStandard = (trueDays / 400).toFixed(4)
 
   const y = parseInt(decimalStandard.substr(0, 2))
@@ -12,8 +12,11 @@ function afiur () {
   const d = parseInt(decimalStandard.substr(4) / 25 + 1)
   const date = `${y}-${`${m}`.padStart(2, '0')}-${`${d}`.padStart(2, '0')}`
 
+  console.log(origin)
+  console.log(current)
   console.log(date)
   console.log(decimalStandard)
+  console.log(trueDays)
 
   return date
 }
